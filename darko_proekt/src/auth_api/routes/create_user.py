50fae9dfc869404c82aee2b68dc0cbca5091a119
laskigvgv <75996913+laskigvgv@users.noreply.github.com>
2023-11-07 17:24:29 +0100

    Delete darko_proekt directory

--- darko_proekt/src/auth_api/routes/create_user.py
+++
@@ -1,6 +0,0 @@
-from .. import auth_blueprint as bp
-
-
-@bp.route("/create", methods=["POST"])
-def create_user():
-    return "Create User API"
--- darko_proekt/src/catalogue_api/routes/get_catalogue.py
+++
@@ -1,6 +0,0 @@
-from .. import catalogue_blueprint as bp
-
-
-@bp.route("/", methods=["GET"])
-def get_catalogue():
-    return "Catalogue API"
