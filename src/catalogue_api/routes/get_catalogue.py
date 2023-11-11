caaf6dac76befddb1a42b142ff8519166662cbea
Lazar Zdravkoski <lazar.zdravkoski@pixyle.ai>
2023-11-11 03:00:05 +0100

    added get catalogue endpoint

--- src/catalogue_api/routes/get_catalogue.py
+++ src/catalogue_api/routes/get_catalogue.py
@@ -1,6 +1,38 @@
+from src.utils.extras import validate_data, mongo_connection
+from flask import abort, current_app
 from .. import catalogue_blueprint as bp
 
 
-@bp.route("/zemi", methods=["GET"])
+@bp.route("/get", methods=["GET"])
 def get_catalogue():
-    return "Catalogue API"
+    """
+    Endpoint to get the catalogue
+    :method: GET
+    :input: None
+
+    :return: JSON response
+        {
+            "catalogue": [
+                {
+                    "category": "product category",
+                    "name": "product name",
+                    "price": "product price",
+                    "quantity": "product quantity"
+                    "description": "product description"
+                    "image_url": "product image url"
+                }
+            ]
+        }
+    """
+
+    # connection to MongoDB
+    mongo_client = mongo_connection()
+    collection = mongo_client.store.products
+
+    # get all products from the database
+    products = collection.find({}, {"_id": 0})
+
+    # return products
+    return {
+        "catalogue": list(products),
+    }
